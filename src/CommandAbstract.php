9d284e9e5d4eb364b121ad0c83e61342213e0eff
Anton Ukhanev <xedin.unknown@gmail.com>
2016-04-03 19:26:37 +0200

    Added `CommandAbstract` class with test

---
+++ src/CommandAbstract.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Dhii\Shell;
+
+use Dhii\ShellInterop;
+use Dhii\ShellCommandInterop;
+
+/**
+ * Common functionality for shell commands.
+ *
+ * @author Dhii Team <development@dhii.co>
+ */
+abstract class CommandAbstract implements
+    ShellInterop\CommandInterface,
+    ShellCommandInterop\ConfigurableCommandInterface
+{
+    protected $workingDirectory;
+    protected $environment;
+    protected $options = array();
+
+    ## Implementation of CommandInterface ######################################
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getWorkingDirectory()
+    {
+        return $this->workingDirectory;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getEnvironment()
+    {
+        return $this->environment;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    ## Implementation of ConfigurableCommandInterface ##########################
+
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function setWorkingDirectory($directoryPath)
+    {
+        $this->workingDirectory = (string) $directoryPath;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function setEnvironment($environmentVars)
+    {
+        if (!is_array($environmentVars) && !is_null($environmentVars)) {
+            throw new \InvalidArgumentException('Could not set environment vars: argument must be an array or null');
+        }
+
+        $this->environment = $environmentVars;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function setOptions($optionVars)
+    {
+        if (!is_array($optionVars)) {
+            throw new \InvalidArgumentException('Could not set option vars: argument must be an array');
+        }
+
+        $this->options = $optionVars;
+
+        return $this;
+    }
+}
---
+++ tests/unit/CommandAbstractTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Dhii\Shell\Test;
+
+use Dhii\Shell;
+
+/**
+ * Description of CommandAbstractTest.
+ *
+ * @author Dhii Team <development@dhii.co>
+ */
+class CommandAbstractTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @since [*next-version*]
+     *
+     * @return Shell\CommandAbstract
+     */
+    public function createInstance()
+    {
+        $commandMock = $this->getMockForAbstractClass('\Dhii\Shell\CommandAbstract');
+
+        return $commandMock;
+    }
+
+    /**
+     * @see http://stackoverflow.com/a/4356295/565229
+     * @since [*next-version*]
+     *
+     * @param int $length
+     *
+     * @return string The pseudo-random string.
+     */
+    public function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; ++$i) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetSetWorkingDirectory()
+    {
+        $command = $this->createInstance();
+        $value = $this->generateRandomString();
+        $command->setWorkingDirectory($value);
+        $this->assertEquals($value, $command->getWorkingDirectory(), 'Working directory must be settable and retrievable');
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetSetWorkingDirectoryDefault()
+    {
+        $command = $this->createInstance();
+        $value = null;
+        $command->setWorkingDirectory($value);
+        $this->assertEquals($value, $command->getWorkingDirectory(), 'Working directory must be settable and retrievable');
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetWorkingDirectoryDefault()
+    {
+        $command = $this->createInstance();
+        $this->assertEquals(null, $command->getWorkingDirectory(), 'Working directory must be null by default');
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetSetEnvVars()
+    {
+        $command = $this->createInstance();
+        $value = [
+            'ENV_VAR_1' => $this->generateRandomString(),
+            'ENV_VAR_2' => $this->generateRandomString(),
+        ];
+        $command->setEnvironment($value);
+        $this->assertEquals($value, $command->getEnvironment(), 'Environment vars must be settable and retrievable');
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetSetEnvVarsDefault()
+    {
+        $command = $this->createInstance();
+        $value = null;
+        $command->setEnvironment($value);
+        $this->assertEquals($value, $command->getEnvironment(), 'Environment vars must be settable and retrievable');
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetEnvVarsDefault()
+    {
+        $command = $this->createInstance();
+        $this->assertEquals(null, $command->getEnvironment(), 'Environment must be null by default');
+    }
+
+    public function testCanGetOptionsDefault()
+    {
+        $command = $this->createInstance();
+        $this->assertEquals([], $command->getOptions(), 'Options must be an empty array by default');
+    }
+
+    /**
+     * @since [*next-version*]
+     */
+    public function testCanGetSetOptions()
+    {
+        $command = $this->createInstance();
+        $value = [
+            'option_1' => $this->generateRandomString(),
+            'option_2' => $this->generateRandomString(),
+        ];
+        $command->setOptions($value);
+        $this->assertEquals($value, $command->getOptions(), 'Options must be settable and retrievable');
+    }
+}
