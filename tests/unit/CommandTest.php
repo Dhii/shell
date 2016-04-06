15c338e4f9df6d9a21d92b1a9c78a496d4c9321f
Anton Ukhanev <xedin.unknown@gmail.com>
2016-04-06 21:20:43 +0200

    Added object-oriented command style tests

--- tests/unit/CommandTest.php
+++ tests/unit/CommandTest.php
@@ -2,6 +2,8 @@
 
 namespace Dhii\Shell\Test;
 
+use Dhii\Shell\Command;
+
 /**
  * Description of CommandTest.
  *
@@ -204,7 +206,7 @@ class CommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testCanSetGetArguments()
     {
-        $arguments = ['/var/log', './'];
+        $arguments = ['--human-readable', '--all'];
         $command = $this->createInstance();
         foreach ($arguments as $_argument) {
             $command->addArgument($_argument);
@@ -435,6 +437,372 @@ class CommandTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             sprintf('%1$s %2$s %3$s %4$s %5$s', $mainCommand, implode(' ', $subCommands), implode(' ', $flags), implode(' ', $arguments), implode(' ', $parameters)),
+            (string) $command,
+            'Must be able to output the command string correctly, including the sub command, flags, arguments, and parameters'
+        );
+    }
+
+    ############################################################################
+    #   Object Oriented Values Tested Below
+    ############################################################################
+
+    /**
+     * Tests whether a single sub command object can be set, and retrieved in uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetSubCommand()
+    {
+        $mainCommand = 'composer';
+        $subCommand = new Command\Subcommand('update');
+        $command = $this->createInstance($mainCommand);
+        $command->addSubCommand($subCommand);
+        $this->assertSame([$subCommand], $command->getSubCommands(), 'Must be able to have a list of all subcommand objects retrieved');
+    }
+
+    /**
+     * Tests whether the subcommand text can be output correctly in the command's
+     * final text when the subcommand is an object.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputSubCommand()
+    {
+        $mainCommand = 'composer';
+        $subCommand = new Command\Subcommand('update');
+        $command = $this->createInstance($mainCommand);
+        $command->addSubCommand($subCommand);
+        $this->assertEquals(sprintf('%1$s %2$s', $mainCommand, $subCommand), (string) $command, 'Must be able to output the command and subcommand string correctly');
+    }
+
+    /**
+     * Tests whether the subcommand text can be output correctly in the command's final text
+     * even if there are multiple sub commands.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputSubCommands()
+    {
+        $mainCommand = 'composer';
+        $subCommands = [new Command\Subcommand('update'), new Command\Subcommand('nao')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($subCommands as $_subCommand) {
+            $command->addSubCommand($_subCommand);
+        }
+        $this->assertEquals(sprintf('%1$s %2$s', $mainCommand, implode(' ', $subCommands)), (string) $command, 'Must be able to output the command and subcommand string correctly');
+    }
+
+    /**
+     * Tests whether a single parameter object can be set, and retrieved in a uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetParameter()
+    {
+        $mainCommand = 'ls';
+        $parameter = new Command\Parameter('/usr/local/bin');
+        $command = $this->createInstance($mainCommand);
+        $command->addParameter($parameter);
+        $this->assertEquals([$parameter], $command->getParameters(), 'Must be able to get and set a single parameter object');
+    }
+
+    /**
+     * Tests whether multiple parameter objects can be set, and retrieved in a uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetParameters()
+    {
+        $mainCommand = 'ls';
+        $parameters = [new Command\Parameter('/usr/local/bin'), new Command\Parameter('../')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($parameters as $_parameter) {
+            $command->addParameter($_parameter);
+        }
+        $this->assertEquals($parameters, $command->getParameters(), 'Must be able to get and set multiple parameter objects');
+    }
+
+    /**
+     * Tests whether the single parameters text can be output correctly in the command's final text.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputParameter()
+    {
+        $mainCommand = 'ls';
+        $parameter = new Command\Parameter('/usr/local/bin');
+        $command = $this->createInstance($mainCommand);
+        $command->addParameter($parameter);
+        $this->assertEquals(sprintf('%1$s %2$s', $mainCommand, $parameter), (string) $command, 'Must be able to output the command and parameter string correctly');
+    }
+
+    /**
+     * Tests whether the multiple parameters text can be output correctly in the command's final text.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputParameters()
+    {
+        $mainCommand = 'ls';
+        $parameters = [new Command\Parameter('/usr/local/bin'), new Command\Parameter('../')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($parameters as $_parameter) {
+            $command->addParameter($_parameter);
+        }
+        $this->assertEquals(sprintf('%1$s %2$s', $mainCommand, implode(' ', $parameters)), (string) $command, 'Must be able to output the command and parameters string correctly');
+    }
+
+    /**
+     * Tests whether a single argument can be set, and retrieved in a uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetArgument()
+    {
+        $argument = new Command\Argument('all');
+        $command = $this->createInstance();
+        $command->addArgument($argument);
+        $this->assertSame([$argument], $command->getArguments(), 'Must be able to get and set a single argument object');
+    }
+
+    /**
+     * Tests whether multiple arguments can be set, and retrieved in a uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetArguments()
+    {
+        $arguments = [new Command\Argument('all'), new Command\Argument('list')];
+        $command = $this->createInstance();
+        foreach ($arguments as $_argument) {
+            $command->addArgument($_argument);
+        }
+        $this->assertSame($arguments, $command->getArguments(), 'Must be able to get and set multiple argument objects');
+    }
+
+    /**
+     * Tests whether the single argument text can be output correctly in the command's final text.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputArgument()
+    {
+        $mainCommand = 'ls';
+        $argument = new Command\Argument('all');
+        $command = $this->createInstance($mainCommand);
+        $command->addArgument($argument);
+        $this->assertEquals(sprintf('%1$s --%2$s', $mainCommand, $argument->getName()), (string) $command, 'Must be able to output the command and argument string correctly');
+    }
+
+    /**
+     * Tests whether the single argument text can be output correctly in the command's final text,
+     * even if the argument has a value.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputArgumentWithValue()
+    {
+        $mainCommand = 'ls';
+        $argument = new Command\Argument('all', 'yes');
+        $command = $this->createInstance($mainCommand);
+        $command->addArgument($argument);
+        $this->assertEquals(sprintf('%1$s --all %2$s', $mainCommand, escapeshellarg('yes')), (string) $command, 'Must be able to output the command and argument string correctly');
+    }
+
+    /**
+     * Tests whether the multiple arguments text can be output correctly in the command's final text.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputArguments()
+    {
+        $mainCommand = 'ls';
+        $arguments = [new Command\Argument('all'), new Command\Argument('list')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($arguments as $_argument) {
+            $command->addArgument($_argument);
+        }
+        $this->assertEquals(sprintf('%1$s --all --list', $mainCommand), (string) $command, 'Must be able to output the command and argument string correctly');
+    }
+
+    /**
+     * Tests whether the multiple arguments text can be output correctly in the command's final text,
+     * even if the arguments have values.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputArgumentsWithValues()
+    {
+        $mainCommand = 'ls';
+        $arguments = [new Command\Argument('all', 'yes'), new Command\Argument('list', 'of course')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($arguments as $_argument) {
+            $command->addArgument($_argument);
+        }
+        $this->assertEquals(sprintf('%1$s --all %2$s --list %3$s', $mainCommand, escapeshellarg('yes'), escapeshellarg('of course')), (string) $command, 'Must be able to output the command and argument string correctly');
+    }
+
+    /**
+     * Tests whether a single flag object can be set, and retrieved in a uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetFlag()
+    {
+        $flag = new Command\Flag('r');
+        $command = $this->createInstance();
+        $command->addFlag($flag);
+        $this->assertSame([$flag], $command->getFlags(), 'Must be able to get and set a single flag object');
+    }
+
+    /**
+     * Tests whether multiple flags can be set, and retrieved in a uniform way.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanSetGetFlags()
+    {
+        $flags = [new Command\Flag('r'), new Command\Flag('f')];
+        $command = $this->createInstance();
+        foreach ($flags as $_flag) {
+            $command->addFlag($_flag);
+        }
+        $this->assertSame($flags, $command->getFlags(), 'Must be able to get and set multiple flag objects');
+    }
+
+    /**
+     * Tests whether the single flag text can be output correctly in the command's final text.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputFlag()
+    {
+        $mainCommand = 'ls';
+        $flag = new Command\Flag('r');
+        $command = $this->createInstance($mainCommand);
+        $command->addFlag($flag);
+        $this->assertEquals(sprintf('%1$s -r', $mainCommand), (string) $command, 'Must be able to output the command and flag string correctly');
+    }
+
+    /**
+     * Tests whether the single flag text can be output correctly in the command's final text,
+     * even if the flag has a value.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputFlagWithValue()
+    {
+        $mainCommand = 'rm';
+        $flag = new Command\Flag('r', 'my_document');
+        $command = $this->createInstance($mainCommand);
+        $command->addFlag($flag);
+        $this->assertEquals(sprintf('%1$s -r %2$s', $mainCommand, escapeshellarg('my_document')), (string) $command, 'Must be able to output the command and flag string correctly');
+    }
+
+    /**
+     * Tests whether the multiple flags text can be output correctly in the command's final text.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputFlags()
+    {
+        $mainCommand = 'rm';
+        $flags = [new Command\Flag('r'), new Command\Flag('f')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($flags as $_flag) {
+            $command->addFlag($_flag);
+        }
+        $this->assertEquals(sprintf('%1$s -r -f', $mainCommand), (string) $command, 'Must be able to output the command and flags string correctly');
+    }
+
+    /**
+     * Tests whether the multiple flags text can be output correctly in the command's final text,
+     * even if flags have values.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputFlagsWithValues()
+    {
+        $mainCommand = 'rm';
+        $flags = [new Command\Flag('r', 'remove'), new Command\Flag('f', 'force')];
+        $command = $this->createInstance($mainCommand);
+        foreach ($flags as $_flag) {
+            $command->addFlag($_flag);
+        }
+        $this->assertEquals(sprintf('%1$s -r %2$s -f %3$s', $mainCommand, escapeshellarg('remove'), escapeshellarg('force')), (string) $command, 'Must be able to output the command and flags string correctly');
+    }
+
+    /**
+     * Tests whether all the possible parts of the command can be output together,
+     * and in the correct order, with correct spacing.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputAll()
+    {
+        $mainCommand = 'composer';
+        $command = $this->createInstance($mainCommand);
+
+        $subCommands = [new Command\Subcommand('update')];
+        foreach ($subCommands as $_subCommand) {
+            $command->addSubCommand($_subCommand);
+        }
+
+        $flags = [new Command\Flag('n')];
+        foreach ($flags as $_flag) {
+            $command->addFlag($_flag);
+        }
+
+        $arguments = [new Command\Argument('no-dev')];
+        foreach ($arguments as $_argument) {
+            $command->addArgument($_argument);
+        }
+
+        $parameters = [new Command\Parameter('monolog/monolog')];
+        foreach ($parameters as $_parameter) {
+            $command->addParameter($_parameter);
+        }
+
+        $this->assertEquals(
+            sprintf('%1$s update -n --no-dev %2$s', $mainCommand, escapeshellarg('monolog/monolog')),
+            (string) $command,
+            'Must be able to output the command string correctly, including the sub command, flags, arguments, and parameters'
+        );
+    }
+
+    /**
+     * Tests whether all the possible parts of the command can be output together,
+     * and in the correct order, with correct spacing.
+     *
+     * @since [*next-version*]
+     */
+    public function testOoCanOutputAllWithValues()
+    {
+        $mainCommand = 'composer';
+        $command = $this->createInstance($mainCommand);
+
+        $subCommands = [new Command\Subcommand('update')];
+        foreach ($subCommands as $_subCommand) {
+            $command->addSubCommand($_subCommand);
+        }
+
+        $flags = [new Command\Flag('n', 'trololo')];
+        foreach ($flags as $_flag) {
+            $command->addFlag($_flag);
+        }
+
+        $arguments = [new Command\Argument('no-dev', 'lalala')];
+        foreach ($arguments as $_argument) {
+            $command->addArgument($_argument);
+        }
+
+        $parameters = [new Command\Parameter('monolog/monolog')];
+        foreach ($parameters as $_parameter) {
+            $command->addParameter($_parameter);
+        }
+
+        $this->assertEquals(
+            sprintf('%1$s update -n %3$s --no-dev %4$s %2$s', $mainCommand, escapeshellarg('monolog/monolog'), escapeshellarg('trololo'), escapeshellarg('lalala')),
             (string) $command,
             'Must be able to output the command string correctly, including the sub command, flags, arguments, and parameters'
         );
